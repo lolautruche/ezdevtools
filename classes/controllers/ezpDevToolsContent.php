11e943284176ae56d59bd4d1b219d3e0aa15eaa1
Jérôme Vieilledent <lolautruche@gmail.com>
2011-11-16 16:48:27 +0100

    Oops, forgot a save :-P

--- classes/controllers/ezpDevToolsContent.php
+++ classes/controllers/ezpDevToolsContent.php
@@ -39,63 +39,9 @@ class ezpDevToolsContent extends ezpRestMvcController
     public function doListClasses()
     {
         $result = new ezpRestMvcResult;
-        $finalClassGroups = array();
-
-        $classGroupList = eZContentClassGroup::fetchList();
-        if ( is_array( $classGroupList ) )
-        {
-            foreach ( $classGroupList as $classGroup )
-            {
-                $groupName = $classGroup->attribute( 'name' );
-                $finalClassGroups[$groupName] = array();
-                $classList = eZContentClassClassGroup::fetchClassList(
-                    eZContentClass::VERSION_STATUS_DEFINED,
-                    $classGroup->attribute( 'id' )
-                );
-                foreach ( $classList as $class )
-                {
-                    $classItem = array(
-                        'name' => $class->attribute( 'name' ),
-                        'identifier' => $class->attribute( 'identifier' ),
-                        'id' => (int)$class->attribute( 'id' ),
-                        'isContainer' => (bool)$class->attribute( 'is_container' ),
-                        'alwaysAvailable' => (bool)$class->attribute( 'always_available' ),
-                        'contentObjectName' => $class->attribute( 'contentobject_name' ),
-                        'remoteId' => $class->attribute( 'remote_id' ),
-                        'fields' => $this->getFieldsForClass( $class )
-                    );
-
-                    $finalClassGroups[$groupName][] = $classItem;
-                }
-            }
-        }
-
         $result->variables['contentClasses'] = $this->model->getContentClassesList();
         return $result;
     }
 
-    /**
-     * Returns fields information for $class
-     *
-     * @param eZContentClass $class
-     * @return array
-     */
-    private function getFieldsForClass( eZContentClass $class )
-    {
-        $fields = array();
-        foreach ( $class->attribute( 'data_map' ) as $identifier => $classAttribute )
-        {
-            $fields[$identifier][] = array(
-                'id' => (int)$classAttribute->attribute( 'id' ),
-                'name' => $classAttribute->attribute( 'name' ),
-                'type' => $classAttribute->attribute( 'data_type_string' ),
-                'isRequired' => (bool)$classAttribute->attribute( 'is_required' ),
-                'isInformationCollector' => (bool)$classAttribute->attribute( 'is_information_collector' ),
-                'isSearchable' => (bool)$classAttribute->attribute( 'is_searchable' ),
-                'isTranslatable' => (bool)$classAttribute->attribute( 'can_translate' )
-            );
-        }
 
-        return $fields;
-    }
 }
