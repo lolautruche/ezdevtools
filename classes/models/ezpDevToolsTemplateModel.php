9f07472ea1e9617c9aa26df32a557b4c2d8d2e70
Jérôme Vieilledent <lolautruche@gmail.com>
2011-11-21 12:38:07 +0100

    Added more information in operator list

--- classes/models/ezpDevToolsTemplateModel.php
+++ classes/models/ezpDevToolsTemplateModel.php
@@ -20,15 +20,55 @@ class ezpDevToolsTemplateModel
     }
 
     /**
-     * Returns a sorted list of template operators
+     * Returns a sorted list of template operators.
+     * Each operator has some information :
+     *  - inputAsParameter: Indicates if operator can/must be used with the pipe syntax
+     *                      by taking the input variable as first parameter. (i.e. $myString|i18n( ... ))
+     *                      Values can be either true, false, null (unknown) or "always" (in this case, this syntax is mandatory)
+     *  - isPHPFunction: Indicates if operator is a wrapper for a native PHP function.
+     *                   Values can be true or false
      *
      * @return array
-     * @todo Return more useful info like operator hints
      */
     public function getOperatorsList()
     {
-        $operators = array_keys( $this->tpl->Operators );
-        sort( $operators );
+        $operators = array();
+        $operatorObjects = array();
+        foreach ( $this->tpl->Operators as $operatorName => $operatorDefinition )
+        {
+            if ( isset( $operatorDefinition['class'] ) )
+            {
+                $operatorClass = $operatorDefinition['class'];
+                if ( !isset( $operatorObjects[$operatorClass] ) )
+                    $operatorObjects[$operatorClass] = new $operatorClass();
+
+                $operatorObject = $operatorObjects[$operatorClass];
+                $operators[$operatorName] = array(
+                    'inputAsParameter' => null,
+                    'isPHPFunction' => false
+                );
+                if ( method_exists( $operatorObject, 'operatorTemplateHints' ) )
+                {
+                    $hints = $operatorObject->operatorTemplateHints();
+                    if ( isset( $hints[$operatorName] ) )
+                    {
+                        if ( isset( $hints[$operatorName]['input-as-parameter'] ) )
+                            $operators[$operatorName]['inputAsParameter'] = $hints[$operatorName]['input-as-parameter'];
+                    }
+                }
+            }
+            else if ( isset( $operatorDefinition['function'] ) && $operatorDefinition['function'] === 'eZPHPOperatorInit' )
+            {
+                $operators[$operatorName] = array(
+                    'inputAsParameter' => true,
+                    'isPHPFunction' => true
+                );
+            }
+
+            $operators[$operatorName]['link'] = "/template/operator/$operatorName/params";
+        }
+
+        ksort( $operators );
         return $operators;
     }
 
